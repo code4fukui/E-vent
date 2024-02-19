27d2a944c1be973f70f08a31979f97661e626f14
ninja03 <69106571+ninja03@users.noreply.github.com>
2024-02-19 11:46:03 +0000

    開発環境でもcronを動くようにした

--- dev.ts
+++ dev.ts
@@ -4,5 +4,11 @@ import dev from "$fresh/dev.ts";
 import config from "./fresh.config.ts";
 
 import "$std/dotenv/load.ts";
+import { init } from "./init.ts";
+
+const isBuildMode = Deno.args.includes("build");
+if (!isBuildMode) {
+  init();
+}
 
 await dev(import.meta.url, "./main.ts", config);
