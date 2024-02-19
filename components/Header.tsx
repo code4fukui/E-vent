ebb9e7f8d85e30bcd0ce6fc3a07b6d7527ff4ff9
haruyuki@chrome-penguin <haruyuki@relicsnow.info>
2024-02-19 20:17:36 +0900

    👍 リンクのhoverで色が変わるように

--- components/Header.tsx
+++ components/Header.tsx
@@ -6,13 +6,21 @@ export default function Header() {
       </a>
       <nav class="ml-auto">
         <ul class="flex items-center">
-          <li>
-            <a href="/application" class="hover:text-orange-500">
+          <li class="mr-3">
+            <a
+              href="/application"
+              class="hover:text-orange-500 hover:underline font-bold"
+            >
               イベントを開催する
             </a>
           </li>
           <li>
-            <a href="/permit" class="hover:text-orange-500">（自治体用）</a>
+            <a
+              href="/permit"
+              class="hover:text-orange-500 hover:underline font-bold"
+            >
+              （自治体用）
+            </a>
           </li>
         </ul>
       </nav>
