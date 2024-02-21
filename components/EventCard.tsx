3295ca057ebf8a0a4052a7e21961d696245ed2c5
haruyuki@chrome-penguin <haruyuki@relicsnow.info>
2024-02-22 00:31:27 +0900

    👍 フォーカスでもホバーのように視覚変化を起こすように

--- components/EventCard.tsx
+++ components/EventCard.tsx
@@ -3,7 +3,10 @@ import { EventItem } from "../interface/EventItem.interface.ts";
 export default function EventCard(props: { event: EventItem }) {
   return (
     <a
-      class="w-full border-solid border-2 rounded-2xl border-orange-500 p-4"
+      class="w-full border-solid border-2 rounded-2xl 
+        border-gray-500 p-4 transition-colors 
+        hover:border-orange-500 hover:bg-gray-200
+        focus:border-orange-500 focus:bg-gray-200"
       href={`/event/${props.event.hash}`}
     >
       <img src={props.event.thumbnailUrl} alt={props.event.title} />
