3afe7007a87522cbb43316b82314ed1e68732c34
haruyuki@chrome-penguin <haruyuki@relicsnow.info>
2024-02-22 21:53:41 +0900

    👍 EventItem interfaceをアップデート

--- interface/EventItem.interface.ts
+++ interface/EventItem.interface.ts
@@ -1,8 +1,21 @@
+export interface JoinPost {
+  body: string;
+  user: string;
+}
+
+export interface CommentPost {
+  body: string;
+  user: string;
+}
+
 export interface EventItem {
+  hash: string;
   title: string;
   date: Date;
+  joinDeadline: Date;
   description: string;
   placement: string;
-  hash: string;
   thumbnailUrl: string;
+  joinners: JoinPost[];
+  comments: CommentPost[];
 }
