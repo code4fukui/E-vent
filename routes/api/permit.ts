6c3763209cff927c791fc4fb95c4f96b1b9e676b
ninja03 <69106571+ninja03@users.noreply.github.com>
2024-02-23 16:27:34 +0900

    自治体がイベントを承認したらついイートする

--- routes/api/permit.ts
+++ routes/api/permit.ts
@@ -1,6 +1,7 @@
 import { FreshContext, Handlers } from "$fresh/server.ts";
 import { kv } from "../../kv.ts";
 import { EventItem } from "../../interface/EventItem.interface.ts";
+import { addNote } from "../../activity_pub.ts";
 
 export const handler: Handlers = {
   async GET(_req: Request, ctx: FreshContext) {
@@ -26,6 +27,29 @@ export const handler: Handlers = {
       ...target,
       permitted: permitMode === "permit",
     });
+
+    // 募集ツイート
+    const messageId1 = crypto.randomUUID();
+    const messageBody1 = permitTarget.title + "の参加者を募集！";
+    await kv.set(["messages", messageId1], {
+      id: messageId1,
+      body: messageBody1,
+      event: permitTarget.hash,
+      type: "joinner",
+    });
+    await addNote(messageId1, messageBody1);
+
+    // 感想募集ツイート
+    const messageId2 = crypto.randomUUID();
+    const messageBody2 = permitTarget.title + "の感想を募集！";
+    await kv.set(["messages", messageId2], {
+      id: messageId2,
+      body: messageBody2,
+      event: permitTarget.hash,
+      type: "comment",
+    });
+    await addNote(messageId2, messageBody2);
+
     return new Response(JSON.stringify({ success: true }), {
       status: 200,
       headers: {
