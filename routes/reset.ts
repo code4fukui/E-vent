66e79a6f4595b011968528eb2855e1ed2f8dcc9c
ninja03 <69106571+ninja03@users.noreply.github.com>
2024-02-23 17:36:46 +0900

    DBリセット機能にパスワード設定

--- routes/reset.ts
+++ routes/reset.ts
@@ -1,10 +1,27 @@
 import { Handlers } from "$fresh/server.ts";
 import { Follower, kv, Message } from "../kv.ts";
 import { EventItem } from "../interface/EventItem.interface.ts";
+import { load } from "https://deno.land/std@0.191.0/dotenv/mod.ts";
+
+await load();
 
 /** DBをリセット */
 export const handler: Handlers = {
-  async GET(_req, _ctx) {
+  async GET(req, _ctx) {
+    const ADMIN_USER = Deno.env.get("ADMIN_USER");
+    const ADMIN_PASSWORD = Deno.env.get("ADMIN_PASSWORD");
+    if (!ADMIN_USER || !ADMIN_PASSWORD) {
+      return new Response(null, { status: 500 });
+    }
+    if (
+      req.headers.get("Authorization") !==
+        `Basic ${btoa(`${ADMIN_USER}:${ADMIN_PASSWORD}`)}`
+    ) {
+      const headers = new Headers({
+        "WWW-Authenticate": 'Basic realm="Fake Realm"',
+      });
+      return new Response("Unauthorized", { status: 401, headers });
+    }
     for await (const message of kv.list<Message>({ prefix: ["messages"] })) {
       await kv.delete(message.key);
     }
