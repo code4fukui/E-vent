511d589e5f593b87e6063053e7b61d2bd9378ed3
haruyuki@chrome-penguin <haruyuki@relicsnow.info>
2024-02-23 18:05:52 +0900

    👍 不要なlogを削除

--- islands/EventCard.tsx
+++ islands/EventCard.tsx
@@ -30,7 +30,6 @@ export default function EventCard(
 
   const genDateString = (dateString: Date) => {
     const date = new Date(dateString);
-    console.log(date, typeof date);
     return `${date.getFullYear()}/${
       (date.getMonth() + 1).toString().padStart(2, "0")
     }/${date.getDate().toString().padStart(2, "0")} ${
