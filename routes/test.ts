799600bfc2112240707e6f16e63a9cf956d09609
ninja03 <69106571+ninja03@users.noreply.github.com>
2024-02-23 16:02:20 +0900

    テスト投稿

--- routes/test.ts
+++ routes/test.ts
@@ -1,10 +1,45 @@
 import { Handlers } from "$fresh/server.ts";
-import { teiki } from "../cron.ts";
+import { kv } from "../kv.ts";
+import { addNote } from "../activity_pub.ts";
 
 /** 動作確認用テスト */
 export const handler: Handlers = {
   async GET(_req, _ctx) {
-    await teiki();
-    return new Response("投稿しました");
+    // イベントを作成
+    const hash = crypto.randomUUID();
+    const title = "テストイベント " + new Date();
+    const eventItem = {
+      hash,
+      title,
+      date: new Date("2024-02-21 16:40:23"),
+      description: "テストイベント",
+      placement: "jigオフィス",
+      thumbnailUrl: "",
+    };
+
+    // 募集メッセージを投稿
+    const messageId1 = crypto.randomUUID();
+    const messageBody1 = eventItem.title + " 募集";
+    await kv.set(["eventItems", eventItem.hash], eventItem);
+    await kv.set(["messages", messageId1], {
+      id: messageId1,
+      body: messageBody1,
+      type: "joinner",
+    });
+    await kv.set(["eventItemsByMessage", messageId1], eventItem.hash);
+    await addNote(messageId1, messageBody1);
+
+    // 感想メッセージを投稿
+    const messageId2 = crypto.randomUUID();
+    const messageBody2 = eventItem.title + " 感想";
+    await kv.set(["messages", messageId2], {
+      id: messageId2,
+      body: messageBody2,
+      type: "comment",
+    });
+    await kv.set(["eventItemsByMessage", messageId2], eventItem.hash);
+    await addNote(messageId2, messageBody2);
+
+    return new Response("テスト投稿をしました");
   },
 };
