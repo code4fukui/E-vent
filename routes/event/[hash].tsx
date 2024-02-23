435934f18c06a5a6f8fa9deafda834ae150d2165
haruyuki@chrome-penguin <haruyuki@relicsnow.info>
2024-02-23 15:50:25 +0900

    👍 イベント詳細の表示を調整

--- routes/event/[hash].tsx
+++ routes/event/[hash].tsx
@@ -18,9 +18,41 @@ export default function Event(event: PageProps<EventItem>) {
     <Fragment>
       <Header />
       <main class="ev-main">
-        <h2 class="ev-title">{item.title}</h2>
-        <img src={item.thumbnailUrl} alt={item.title} />
-        <p>{item.description}</p>
+        <section>
+          <h2 class="ev-title">{item.title}</h2>
+          <img
+            class="w-full h-40 object-cover object-center bg-gray-300"
+            src={item.thumbnailUrl}
+            alt={item.title}
+          />
+          <p class="mt-4">{item.description}</p>
+        </section>
+
+        <section class="mt-4">
+          <h3 class="text-xl font-bold">参加者一覧</h3>
+          {item.joinners !== undefined
+            ? (
+              <ul class="mt-2 flex gap-2">
+                {item.joinners.map((joinner) => {
+                  <span>{joinner.user}</span>;
+                })}
+              </ul>
+            )
+            : <p>まだ参加者がいません</p>}
+        </section>
+
+        <section class="mt-4">
+          <h3 class="text-xl font-bold">コメント</h3>
+          {item.comments !== undefined
+            ? (
+              <ul class="mt-2 flex gap-2">
+                {item.comments.map((joinner) => {
+                  <span>{joinner.user}</span>;
+                })}
+              </ul>
+            )
+            : <p>まだコメントがありません</p>}
+        </section>
       </main>
     </Fragment>
   );
