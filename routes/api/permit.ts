85d4ae7d112f5025a8b2641220ed7c7cf3fef57e
ninja03 <69106571+ninja03@users.noreply.github.com>
2024-02-23 16:29:23 +0900

    エラー修正

--- routes/api/permit.ts
+++ routes/api/permit.ts
@@ -30,22 +30,22 @@ export const handler: Handlers = {
 
     // 募集ツイート
     const messageId1 = crypto.randomUUID();
-    const messageBody1 = permitTarget.title + "の参加者を募集！";
+    const messageBody1 = target.title + "の参加者を募集！";
     await kv.set(["messages", messageId1], {
       id: messageId1,
       body: messageBody1,
-      event: permitTarget.hash,
+      event: target.hash,
       type: "joinner",
     });
     await addNote(messageId1, messageBody1);
 
     // 感想募集ツイート
     const messageId2 = crypto.randomUUID();
-    const messageBody2 = permitTarget.title + "の感想を募集！";
+    const messageBody2 = target.title + "の感想を募集！";
     await kv.set(["messages", messageId2], {
       id: messageId2,
       body: messageBody2,
-      event: permitTarget.hash,
+      event: target.hash,
       type: "comment",
     });
     await addNote(messageId2, messageBody2);
