19f3b26f8da5b1834613aad63412649073bc4c15
ninja03 <69106571+ninja03@users.noreply.github.com>
2024-02-25 13:29:04 +0900

    Update main.ts

--- main.ts
+++ main.ts
@@ -9,5 +9,12 @@ import "$std/dotenv/load.ts";
 import { start } from "$fresh/server.ts";
 import manifest from "./fresh.gen.ts";
 import config from "./fresh.config.ts";
+import { startCron } from "./cron.ts";
+
+const isBuildMode = Deno.args.includes("build");
+if (!isBuildMode) {
+  // cronの処理はこちら
+  startCron();
+}
 
 await start(manifest, config);
