4d983ccc407d7d3e72c42dce459a8f0a33b1c14f
haruyuki@chrome-penguin <haruyuki@relicsnow.info>
2024-02-28 01:00:05 +0900

    👍 イベント承認ページを分けて出すように

--- routes/permit.tsx
+++ routes/permit.tsx
@@ -18,15 +18,55 @@ export const handler: Handlers<EventItem[]> = {
 
 export default function Permit(eventItems: PageProps<EventItem[]>) {
   const items = eventItems.data;
+  const pastEvents = items.filter((item) =>
+    (new Date(item.date)).getTime() < (new Date()).getTime()
+  );
+  const needPermitEvents = items.filter((item) =>
+    (new Date(item.date)).getTime() > (new Date()).getTime() &&
+    item.permitted === undefined
+  );
+  const permittedEvents = items.filter((item) =>
+    (new Date(item.date)).getTime() > (new Date()).getTime() &&
+    item.permitted !== undefined
+  );
   return (
     <Fragment>
       <Header />
       <main class="ev-main">
-        <h2 class="ev-title">イベント承認</h2>
-        <section class="flex flex-col md:grid md:grid-cols-2 gap-2">
-          {items.sort((a, b) =>
-            new Date(b.date).getTime() - new Date(a.date).getTime()
-          ).map((item) => <EventCard event={item} isPermitter={true} />)}
+        <section class="mb-8">
+          <h2 class="ev-title">イベント承認</h2>
+          <div
+            class={"flex flex-col md:grid gap-2" +
+              (needPermitEvents.length > 0
+                ? "md:grid-cols-2"
+                : " md:grid-cols-1")}
+          >
+            {needPermitEvents.length > 0
+              ? needPermitEvents.sort((a, b) =>
+                new Date(b.date).getTime() - new Date(a.date).getTime()
+              ).map((item) => <EventCard event={item} isPermitter={true} />)
+              : (
+                <span class="w-full my-16 text-xl text-center text-gray-700">
+                  未承認のイベントはありません
+                </span>
+              )}
+          </div>
+        </section>
+        <section class="mb-8">
+          <h2 class="ev-title">承認・却下済みイベント</h2>
+          <div class="flex flex-col md:grid md:grid-cols-2 gap-2">
+            {permittedEvents.sort((a, b) =>
+              new Date(b.date).getTime() - new Date(a.date).getTime()
+            ).map((item) => <EventCard event={item} isPermitter={true} />)}
+          </div>
+        </section>
+        <section>
+          <h2 class="ev-title">終了済みイベント</h2>
+          <div class="flex flex-col md:grid md:grid-cols-2 gap-2">
+            {pastEvents.sort((a, b) =>
+              new Date(b.date).getTime() - new Date(a.date).getTime()
+            ).map((item) => <EventCard event={item} isPermitter={true} />)}
+          </div>
         </section>
       </main>
     </Fragment>
