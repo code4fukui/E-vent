805390cea18d646e8e224d938926065b8b1adb1c
haruyuki@chrome-penguin <haruyuki@relicsnow.info>
2024-02-28 00:20:03 +0900

    👍 indexで終了済みイベントと参加者募集中イベントを別表示に

--- routes/index.tsx
+++ routes/index.tsx
@@ -20,17 +20,35 @@ export const handler: Handlers<EventItem[]> = {
 
 export default function Home(eventItems: PageProps<EventItem[]>) {
   const items = eventItems.data;
+  const events = items.filter((item) =>
+    (new Date(item.date)).getTime() > (new Date()).getTime()
+  );
+  const pastEvents = items.filter((item) =>
+    (new Date(item.date)).getTime() < (new Date()).getTime()
+  );
   return (
     <Fragment>
       <Header />
       <main class="ev-main">
-        <h2 class="ev-title">イベント一覧</h2>
-        <section class="flex flex-col md:grid md:grid-cols-2 gap-2">
-          {items.sort((a, b) =>
-            new Date(b.date).getTime() - new Date(a.date).getTime()
-          ).map((
-            item,
-          ) => <EventCard event={item} />)}
+        <section class="mb-16">
+          <h2 class="ev-title">参加者募集中のイベント</h2>
+          <div class="flex flex-col md:grid md:grid-cols-2 gap-2">
+            {events.sort((a, b) =>
+              new Date(b.date).getTime() - new Date(a.date).getTime()
+            ).map((
+              item,
+            ) => <EventCard event={item} />)}
+          </div>
+        </section>
+        <section>
+          <h2 class="ev-title">終了したイベント</h2>
+          <div class="flex flex-col md:grid md:grid-cols-2 gap-2">
+            {pastEvents.sort((a, b) =>
+              new Date(b.date).getTime() - new Date(a.date).getTime()
+            ).map((
+              item,
+            ) => <EventCard event={item} />)}
+          </div>
         </section>
       </main>
     </Fragment>
