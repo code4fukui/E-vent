8bf80ec3a9bb6be359a3d63629cce92fbdc2218f
haruyuki@chrome-penguin <haruyuki@relicsnow.info>
2024-02-29 00:31:19 +0900

    👍 イベント申請時のサムネイル画像を自動リサイズするように

--- islands/ImageInputPreview.tsx
+++ islands/ImageInputPreview.tsx
@@ -2,14 +2,14 @@ import { useState } from "preact/hooks";
 import { JSX } from "preact/jsx-runtime";
 
 export default function ImageInputPreview(props: { name: string }) {
-  const [image, setImage] = useState<string | ArrayBuffer | null | undefined>(
-    undefined,
+  const [image, setImage] = useState<string>(
+    "",
   );
 
-  const onChangeImage = (
+  const onChangeImage = async (
     { currentTarget }: JSX.TargetedEvent<HTMLInputElement, Event>,
   ) => {
-    setImage(undefined);
+    setImage("");
 
     if (!currentTarget.files || currentTarget.files.length < 1) {
       return;
@@ -18,11 +18,40 @@ export default function ImageInputPreview(props: { name: string }) {
     const file = currentTarget.files[0];
     if (!file.type.match("image.*")) return;
 
-    const reader = new FileReader();
-    reader.onload = (e) => {
-      setImage(e.target?.result);
-    };
-    reader.readAsDataURL(file);
+    const imageData = await createImageBitmap(file);
+    const canvas = document.createElement("canvas");
+    canvas.width = 1200;
+    canvas.height = 630;
+    const ctx = canvas.getContext("2d");
+    if (!ctx) {
+      console.warn("getContext failed: can't resize image");
+      alert("画像の読み込みに失敗しました");
+      return;
+    }
+    const landscape = canvas.width > canvas.height;
+    const scale = landscape
+      ? canvas.width / imageData.width
+      : canvas.height / imageData.height;
+    ctx.drawImage(
+      imageData,
+      0,
+      0,
+      imageData.width,
+      imageData.height,
+      landscape ? 0 : (canvas.width - imageData.width * scale) / 2,
+      landscape ? (canvas.height - imageData.height * scale) / 2 : 0,
+      landscape ? canvas.width : imageData.width * scale,
+      landscape ? imageData.height * scale : canvas.height,
+    );
+    console.log(
+      scale,
+      landscape ? 0 : (canvas.width - imageData.width * scale) / 2,
+      landscape ? (canvas.height - imageData.height * scale) / 2 : 0,
+      landscape ? canvas.width : imageData.width * scale,
+      landscape ? imageData.height * scale : canvas.height,
+    );
+    const resizedImageData = canvas.toDataURL("image/png");
+    setImage(resizedImageData);
   };
 
   return (
@@ -34,7 +63,7 @@ export default function ImageInputPreview(props: { name: string }) {
         onInput={(event) => onChangeImage(event)}
       />
       {image
-        ? <img class="w-full object-contain object-center" src={image} />
+        ? <img class="w-full my-4 object-contain object-center" src={image} />
         : ""}
     </div>
   );
