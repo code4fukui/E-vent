bb07a8318684a9572a45a1c0ed8de76b765b5b14
haruyuki@chrome-penguin <haruyuki@relicsnow.info>
2024-02-29 00:44:40 +0900

    👍 サムネイルが自動リサイズされることをツールチップで表示

--- islands/ImageInputPreview.tsx
+++ islands/ImageInputPreview.tsx
@@ -1,5 +1,5 @@
 import { useState } from "preact/hooks";
-import { JSX } from "preact/jsx-runtime";
+import { Fragment, JSX } from "preact/jsx-runtime";
 
 export default function ImageInputPreview(props: { name: string }) {
   const [image, setImage] = useState<string>(
@@ -55,16 +55,34 @@ export default function ImageInputPreview(props: { name: string }) {
   };
 
   return (
-    <div>
-      <input
-        type="file"
-        accept="image/*"
-        name={props.name}
-        onInput={(event) => onChangeImage(event)}
-      />
+    <Fragment>
+      <div class="flex items-center justify-start">
+        <div
+          class="tooltip mr-4"
+          data-tooltip="画像は1200x630ピクセルでリサイズされます"
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+            width="24"
+            height="24"
+          >
+            <path d="M13 7.5a1 1 0 1 1-2 0 1 1 0 0 1 2 0Zm-3 3.75a.75.75 0 0 1 .75-.75h1.5a.75.75 0 0 1 .75.75v4.25h.75a.75.75 0 0 1 0 1.5h-3a.75.75 0 0 1 0-1.5h.75V12h-.75a.75.75 0 0 1-.75-.75Z">
+            </path>
+            <path d="M12 1c6.075 0 11 4.925 11 11s-4.925 11-11 11S1 18.075 1 12 5.925 1 12 1ZM2.5 12a9.5 9.5 0 0 0 9.5 9.5 9.5 9.5 0 0 0 9.5-9.5A9.5 9.5 0 0 0 12 2.5 9.5 9.5 0 0 0 2.5 12Z">
+            </path>
+          </svg>
+        </div>
+        <input
+          type="file"
+          accept="image/*"
+          name={props.name}
+          onInput={(event) => onChangeImage(event)}
+        />
+      </div>
       {image
         ? <img class="w-full my-4 object-contain object-center" src={image} />
         : ""}
-    </div>
+    </Fragment>
   );
 }
